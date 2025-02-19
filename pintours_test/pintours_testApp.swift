42942c100bbdb39ca1e691f008d2ff67adbf5ed2
Obada Alhalabi <abadah.alhalabi2020@gmail.com>
2025-02-19 09:24:26 +0400

    Initial Commit

---
+++ pintours_test/pintours_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  pintours_testApp.swift
+//  pintours_test
+//
+//  Created by Obada Alhalabi on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct pintours_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
